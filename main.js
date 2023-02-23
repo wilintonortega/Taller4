ce05089ff7401d2f04fb57d11da14a58dbf6e006
wili1991 <wikki9106@gmail.com>
2023-02-22 19:18:57 -0500

    lower case en prompt

--- main.js
+++ main.js
@@ -3,19 +3,19 @@
 // Una vez cocinado le pregunto si quiere sal en el huevo.
 // Si me dice que no lo sirvo en el Plato. Si me dice que si le hecho sal y después lo sirvo en el plato.
 
-const huevofrito=prompt("quiere el huevo frito? -- Si o No");
-const qsal=prompt("quiere sal en el Huevo? -- Si o No");
+const huevofrito=prompt("quiere el huevo frito? -- Si o No").toLowerCase();
+const qsal=prompt("quiere sal en el Huevo? -- Si o No").toLowerCase();
 
 let huevo="";
 let sal="";
 
-if(huevofrito=="SI".toLowerCase() && qsal=="SI".toLowerCase()){
+if(huevofrito=="si" && qsal=="si"){
     alert(`Se sirve plato: Huevo frito con Sal`);
 }   
-else if(huevofrito=="NO".toLowerCase() && qsal=="SI".toLowerCase()){
+else if(huevofrito=="no" && qsal=="si"){
     alert(`Se sirve plato: Huevo cocinado con Sal`);
 }
-else if(huevofrito=="SI".toLowerCase() && qsal=="NO".toLowerCase()){
+else if(huevofrito=="si" && qsal=="no"){
     alert(`Se sirve plato: Huevo frito sin sal`);
 }else{
     alert(`Se sirve plato: Huevo cocinado sin Sal `);
