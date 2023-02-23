2633b80b8a361a7c123500420b017e5558a2eaaa
wili1991 <wikki9106@gmail.com>
2023-02-23 12:34:25 -0500

    ejercicio 2 taller 4 con arrays

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ Si hay más de un helado con el mismo precio o por de bajo de precio del usuario
 El programa también tiene que poder dar los vueltos de su compra con su factura
 */
 
-
+/*
 let helados=[
     {
     nombre:"Bonais", 
@@ -101,5 +101,60 @@ function cliente() {
 }
 
 cliente()
+*/
 
 
+/*
+
+2.	3 usuarios entran a una heladería a comprar un helado, pero hay un problema: los precios no están al lado de cada estante con su respectivo helado.
+ “Ellos quieren comprar el helado más caro que puedan con la plata que tienen.
+Miguel tiene $1.000 
+Carlos tiene $2.000 
+Manuel tiene $500 
+Los precios de helados son los siguientes:
+Bonais: $400
+Palito de helado de agua: $1.000 
+Palito de helado de crema: $2.200
+Bombón helado con arequipe: $1.500
+Bombón helado con chispas de chocolate: $2.500
+Bombón helado con fresas: $2.800
+Medio litro de helado: $ 2.000
+
+El programa tiene que pedir el ingreso de su dinero y el nombre de cliente
+Si hay más de un helado con el mismo precio o por de bajo de precio del usuario mostrar las opciones disponibles
+El programa también tiene que poder dar los vueltos de su compra con su factura
+*/
+
+const precio=[400,1000,2200,1500,2500,2800,2000];
+const helado=["Bonais","Palito de helado de agua","Palito de helado de crema","Bombón helado con arequipe","Bombón helado con chispas de chocolate","Bombón helado con fresas","Medio litro de helado"];
+let nvalor=[];
+
+
+for (let i = 0; i < 3; i++) {
+        
+    nombreCliente=prompt("Ingrese el nombre del cliente").toUpperCase();
+    dineroCliente=prompt("Ingrese el dinero del cliente");
+    console.log(`\n ${nombreCliente} Puede comprar con $${dineroCliente}: \n `);
+        for (let j = 0; j < precio.length; j++) {
+            let preciomax=dineroCliente>=precio[j]
+            if(preciomax){
+                console.log(`$${precio[j]}   -   ${helado[j]} \n`);
+
+                //defino variable para insertar datos en nuevo array nvalor
+                var max=nvalor.push(precio[j]);
+            }
+
+            //funcion para ordenar valores
+            nvalor.sort(function(a, b){return a-b});
+
+
+
+        } 
+        let cambio=dineroCliente-(nvalor[nvalor.length-1]);
+        console.log(`El cambio por comprar el helado mas caro de la lista con $${dineroCliente} es: $${cambio}`);
+        nvalor=[];
+
+
+
+
+}
